4e72441fe1a494d9a95739aff679c4436f76631b
Ioannis Fytros <rinenweb@gmail.com>
2025-04-05 18:18:14 +0300

    Update default.php
    just putting some classes to be easier to improve its looks with CSS

--- mod_authorinfo/tmpl/default.php
+++ mod_authorinfo/tmpl/default.php
@@ -3,7 +3,7 @@
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
 use Joomla\Component\Contact\Site\Helper\RouteHelper;
-//use Joomla\Component\Content\Site\Helper\RouteHelper as ContentHelperRoute;
+use Joomla\Component\Content\Site\Helper\RouteHelper as ContentHelperRoute;
 
 $showImage = $params->get('show_image');
 $showMisc = $params->get('show_misc');
@@ -17,54 +17,74 @@ if (!$data) return;
 $contact = $data->contact;
 $articles = $data->articles;
 if ($showArticles && is_array($articles)) {
-    $articleLimit = (int) $params->get('article_limit', 5);
     $articles = array_slice($articles, 0, $articleLimit);
 }
 ?>
-  <?php if ($customCss = trim($params->get('customCss'))): ?>
+
+<?php if ($customCss = trim($params->get('customCss'))): ?>
     <style>
-            <?php echo $customCss; ?>
+        <?php echo $customCss; ?>
     </style>
 <?php endif; ?>
-<?php if ($showImage && $contact->image): ?>
-    <img class="author-photo" src="<?php echo htmlspecialchars($contact->image); ?>" alt="<?php echo htmlspecialchars($contact->name); ?>" />
-<?php if ($showMisc && $contact->misc): ?>
+
 <?php
+$miscOutput = '';
+if ($showMisc && $contact->misc) {
     $cleaned = preg_replace('/\s+/', ' ', $contact->misc);
     $parts = preg_split('/<hr[^>]+id=["\']system-readmore["\'][^>]*>/i', $cleaned, 2);
     $intro = $parts[0];
     $rest = $parts[1] ?? '';
+    ob_start();
+    ?>
+    <div class="author-bio">
+        <?php echo $intro; ?>
+        <?php if (!empty($rest)): ?>
+            <p>
+                <a href="<?php echo Route::_(RouteHelper::getContactRoute($contact->id, $contact->catid)); ?>">
+                    <?php echo sprintf(Text::_('MOD_AUTHORINFO_READ_MORE'), htmlspecialchars($contact->name)); ?>
+                </a>
+            </p>
+        <?php endif; ?>
+    </div>
+    <?php
+    $miscOutput = ob_get_clean();
+}
 ?>
-<div class="author-bio">
-<?php echo $intro; ?>
-<?php if (!empty($rest)): ?>
-<p><a href="<?php echo Route::_(RouteHelper::getContactRoute($contact->id, $contact->catid)); ?>">
-<?php echo sprintf(Text::_('MOD_AUTHORINFO_READ_MORE'), htmlspecialchars($contact->name)); ?>
-</a></p>
-<?php endif; ?>
-</div>
+
+<?php if ($showImage && $contact->image && $miscOutput): ?>
+    <div class="author-info-flex">
+        <div class="author-photo">
+            <img src="<?php echo htmlspecialchars($contact->image); ?>" alt="<?php echo htmlspecialchars($contact->name); ?>" />
+        </div>
+        <?php echo $miscOutput; ?>
+    </div>
+<?php elseif ($showImage && $contact->image): ?>
+    <div class="author-photo">
+        <img src="<?php echo htmlspecialchars($contact->image); ?>" alt="<?php echo htmlspecialchars($contact->name); ?>" />
+    </div>
+<?php elseif ($miscOutput): ?>
+    <?php echo $miscOutput; ?>
 <?php endif; ?>
 
-<?php endif; ?>
 <?php if ($showArticles && !empty($articles)): ?>
-<div class="author-articles">
-<h4><?php echo sprintf(Text::_('MOD_AUTHORINFO_OTHER_ARTICLES'), htmlspecialchars($contact->name)); ?></h4>
-<ul>
-<?php foreach ($articles as $article): ?>
-<li>
-<a href="<?php echo Route::_(ContentHelperRoute::getArticleRoute($article->id, $article->catid)); ?>">
-<?php echo htmlspecialchars($article->title); ?>
-</a>
-</li>
-<?php endforeach; ?>
-</ul>
-</div>
+    <div class="author-articles">
+        <h4><?php echo sprintf(Text::_('MOD_AUTHORINFO_OTHER_ARTICLES'), htmlspecialchars($contact->name)); ?></h4>
+        <ul>
+            <?php foreach ($articles as $article): ?>
+                <li>
+                    <a href="<?php echo Route::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>">
+                        <?php echo htmlspecialchars($article->title); ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 <?php endif; ?>
 
 <?php if ($showForm): ?>
-<p>
-    <a href="<?php echo Route::_(RouteHelper::getContactRoute($contact->id, $contact->catid)); ?>">
-        <?php echo sprintf(Text::_('MOD_AUTHORINFO_CONTACT_LINK'), htmlspecialchars($contact->name)); ?>
-    </a>
-</p>
+    <p>
+        <a href="<?php echo Route::_(RouteHelper::getContactRoute($contact->id, $contact->catid)); ?>">
+            <?php echo sprintf(Text::_('MOD_AUTHORINFO_CONTACT_LINK'), htmlspecialchars($contact->name)); ?>
+        </a>
+    </p>
 <?php endif; ?>
